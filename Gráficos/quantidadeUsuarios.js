a572c6441f7d43649ef9cf3990399d6a1ddb5541
losachi <108551432+losachi@users.noreply.github.com>
2024-09-19 15:09:12 -0300

    rer

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -31,7 +31,7 @@ async function quantidadeUsuarios() {
         xaxis: {
             tickfont: tickConfig,
             title: {
-                text: 'Nome das redes sociais',
+                text: 'Nome dos estados',
                 font: {
                     color: getCSS('--cor-secundaria')
                 }
