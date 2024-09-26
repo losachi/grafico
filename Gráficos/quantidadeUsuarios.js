399ca2886dc9e956f5a8832e7f48b47f3fd4c629
losachi <108551432+losachi@users.noreply.github.com>
2024-09-26 13:37:02 -0300

    rerttt

--- Gráficos/quantidadeUsuarios.js
+++ Gráficos/quantidadeUsuarios.js
@@ -4,7 +4,7 @@ async function quantidadeUsuarios() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url)
     const dados = await res.json()
-    const nomeDasRedes = { }
+    const nomeDasRedes = ['Bahia', 'Minas Gerais','Maranhão', 'Pará', 'Pernambuco']
     const quantidadeUsuarios = Object.values(dados)
     const data = [
         {
