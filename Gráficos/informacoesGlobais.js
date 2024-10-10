e2966b7eb20a43039647b64bcdc9f16aa48bd5fe
losachi <108551432+losachi@users.noreply.github.com>
2024-10-10 13:34:08 -0300

    uu

--- Gráficos/informacoesGlobais.js
+++ Gráficos/informacoesGlobais.js
@@ -6,7 +6,7 @@ async function vizualizarInformacoesGlobais() {
 
   const paragrafo = document.createElement('p')
   paragrafo.classList.add('graficos-container__texto')
-  paragrafo.innerHTML = `Os quilombos, também conhecidos como mocambos, foram comunidades formadas no Brasil durante o período colonial por africanos escravizados e/ou seus descendentes..`
+  paragrafo.innerHTML = `Os quilombos, também conhecidos como mocambos, foram comunidades formadas no Brasil durante o período colonial por africanos escravizados e/ou seus descendentes.`
   
   const container = document.getElementById('graficos-container')
   container.appendChild(paragrafo)
