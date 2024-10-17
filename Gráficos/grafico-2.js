7e04ecf30cbeea98d0122a1ac6de3c513c5bf116
losachi <108551432+losachi@users.noreply.github.com>
2024-10-17 13:28:26 -0300

    sa

--- Gráficos/grafico-2.js
+++ Gráficos/grafico-2.js
@@ -21,7 +21,7 @@ async function quantidadeUsuarios() {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
         title: {
-            text: 'pessoas negras no mercado de trabalho',
+            text: 'Pessoas Negras no Mercado de Trabalho',
             font: {
                 color: getCSS('--cor-principal'),
                 family: getCSS('--fonte'),
